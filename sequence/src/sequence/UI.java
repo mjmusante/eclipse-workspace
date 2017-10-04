a1880266a5131f408cab129c6bc50e6ceab7b8d7
Mark Musante <mark.musante@gmail.com>
2017-10-04 14:59:52 -0400

    auto-formatted code

--- sequence/src/sequence/UI.java
+++ sequence/src/sequence/UI.java
@@ -11,10 +11,9 @@ import javax.swing.JTextField;
 
 import BreezySwing.GBFrame;
 
-
 @SuppressWarnings("serial")
 public class UI extends GBFrame {
-	
+
 	JTextArea mainWin = addTextArea("", 1, 1, 5, 6);
 	JLabel lab1 = addLabel("Enter numbers separated by spaces or commas", 7, 2, 3, 1);
 	JLabel lab2 = addLabel("", 7, 1, 1, 1);
@@ -23,14 +22,14 @@ public class UI extends GBFrame {
 	JButton outputButton = addButton("Output", 9, 2, 1, 1);
 	JButton resetButton = addButton("Reset", 9, 3, 1, 1);
 	JButton exitButton = addButton("Exit", 9, 4, 1, 1);
-	
+
 	Sequencer curSeq = new Sequencer();
 	List<Sequencer> curResult = null;
 	String errMsg = null;
-	
+
 	public UI() {
 		mainWin.setEditable(false);
-		
+
 		inputField.addKeyListener(new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent ke) {
@@ -39,37 +38,39 @@ public class UI extends GBFrame {
 					outputResult();
 				}
 			}
-			
+
 			@Override
-			public void keyReleased(KeyEvent ke) {}
-			
+			public void keyReleased(KeyEvent ke) {
+			}
+
 			@Override
-			public void keyTyped(KeyEvent ke) {}
+			public void keyTyped(KeyEvent ke) {
+			}
 		});
 	}
-	
+
 	private void outputResult() {
 		String output = "";
-		
+
 		if (errMsg != null) {
 			output = output + errMsg + "\n";
 			errMsg = null;
 		}
-		
+
 		output = output + "Current input: ";
 		if (curSeq.size() == 0) {
 			output = output + "<empty>";
 		} else {
 			output = output + curSeq.prettyPrint();
 		}
-		
+
 		if (curResult != null) {
 			output = output + "\n\nLongest sequence";
 			if (curResult.size() > 1) {
 				output = output + "s";
 			}
 			output = output + ": ";
-			
+
 			if (curResult.size() == 0) {
 				output = output + "<none>";
 			} else {
@@ -82,29 +83,28 @@ public class UI extends GBFrame {
 		mainWin.setText(output);
 		inputField.requestFocus();
 	}
-	
+
 	public void reset() {
 		curSeq = new Sequencer();
 		curResult = null;
-		
-		outputResult();		
+
+		outputResult();
 	}
-	
+
 	private void scanSequence(String input) {
 		String[] a = input.split("[ ,]");
 		for (String s : a) {
 			try {
 				if (s.length() > 0)
 					curSeq.add(Integer.parseInt(s));
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				errMsg = "ERROR: input '" + s + "' is not an integer";
 				break;
 			}
 		}
 		curResult = null;
 	}
-	
+
 	public void buttonClicked(JButton b) {
 		if (b == exitButton) {
 			System.exit(0);
@@ -122,13 +122,13 @@ public class UI extends GBFrame {
 		scanSequence(inputField.getText());
 		inputField.setText("");
 	}
-	
+
 	public static void main(String[] argv) {
-		
+
 		UI frm = new UI();
-		
+
 		frm.reset();
-		
+
 		frm.setTitle("Longest Sequence");
 		frm.setSize(800, 600);
 		frm.setVisible(true);
