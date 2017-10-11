609cf432502f7e3f3f6051f2e9429548050ce542
Mark Musante <mark.musante@gmail.com>
2017-10-11 19:14:22 -0400

    added comments

--- sequence/src/sequence/IntegerList.java
+++ sequence/src/sequence/IntegerList.java
@@ -2,11 +2,12 @@ package sequence;
 
 import java.util.Iterator;
 
+
 class IntegerListIterator implements Iterator<Integer> {
 
-	int[] ary;
-	int len;
-	int pos = 0;
+	private int[] ary;
+	private int len;
+	private int pos = 0;
 	
 	public IntegerListIterator(int[] ary, int len) {
 		this.ary = ary;
@@ -26,24 +27,39 @@ class IntegerListIterator implements Iterator<Integer> {
 
 public class IntegerList implements Iterable<Integer> {
 	
+	/* tells us how much to increase the array size each time we hit the limit */
 	private static final int INCR = 16;
 	
-	private int[] ary = new int[INCR];
-	private int count = 0;
-	private int max = INCR;
+	/* the array storage */
+	private int[] ary;
 	
-	public IntegerList() {}
+	/* the number of valid elements in the array */
+	private int count;
 	
+	/* the total number of elements in the array */
+	private int max;
+	
+	/* construct an empty array */
+	public IntegerList() {
+		this.ary = new int[INCR];
+		this.count = 0;
+		this.max = INCR;
+	}
+	
+	/* construct an array and populate it with an existing array */
 	public IntegerList(int[] list) {
+		this();
 		for (int i: list) {
 			this.add(i);
 		}
 	}
 
+	/* return the number of valid entries in the array */
 	public int size() {
 		return count;
 	}
 	
+	/* add one number to the end of the array */
 	public void add(int num) {
 		if (count == max) {
 			// need to grow array
@@ -55,22 +71,26 @@ public class IntegerList implements Iterable<Integer> {
 		ary[count++] = num;
 	}
 	
+	/* append one IntegerList to another */
 	public void addAll(IntegerList list) {
 		for (int i: list) {
 			this.add(i);
 		}
 	}
 	
+	/* append an array of numbers to the array */
 	public void addAll(int[] list) {
 		for (int i: list) {
 			this.add(i);
 		}
 	}
 
+	/* get the value of the entry at a specific position in the array */
 	public int get(int pos) {
 		return ary[pos];
 	}
 
+	/* generate an iterator for the array */
 	@Override
 	public Iterator<Integer> iterator() {
 		// TODO Auto-generated method stub
